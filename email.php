c67ec2b46a889549fa782b4162b082b1cf3cfd09
BatistaCR <alexbatistacr@gmail.com>
2021-07-21 17:01:33 -0600

    Colores e imagenes

--- email.php
+++ email.php
@@ -3,18 +3,20 @@ $to = "bsdesarrollo.cr@gmail.com";
 $subject = "Contacto sitio web";
 $txt = ($_POST['comentario']);
 $headers = "From:" . ($_POST['correo']) . "\r\n";
-header('Location: index.php');
 
-     if(@mail($to, $subject, $txt, $headers))
-     {
-        echo '<script language="javascript">';
-        echo 'alert("El mensaje ha sido enviado")';
-        echo '</script>';
-      }
-     else{
-        echo '<script language="javascript">';
-        echo 'alert("El mensaje no se pudo enviar")';
-        echo '</script>';
-     }
-     exit;
+if(@mail($to, $subject, $txt, $headers))
+{
+   echo'<script type="text/javascript">
+   alert("Tu mensaje a sido enviado 😁");
+   window.location.href="index.php";
+   </script>';
+ }
+ 
+else{
+   echo'<script type="text/javascript">
+   alert("Uuups, parece que hubo un error 🙁");
+   window.location.href="index.php";
+   </script>';
+}
+exit;
 ?>
